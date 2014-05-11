6f6803b5a8d92760648badda7ec6caeae751c834
Evan Baumgardner <baumer@Goliath.local>
2014-05-11 13:26:44 -0700

    Initial Commit

---
+++ homework2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  homework2
+//
+//  Created by Evan Baumgardner on 5/11/14.
+//  Copyright (c) 2014 Evan Baumgardner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ homework2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  homework2
+//
+//  Created by Evan Baumgardner on 5/11/14.
+//  Copyright (c) 2014 Evan Baumgardner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
